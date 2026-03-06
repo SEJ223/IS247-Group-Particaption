6a8609b4a78531814ca4d33789b2b3928e6d7681
Saunders <sebuwei1@umbc.edu>
2026-03-06 10:09:10 -0500

    Added a comment to the code

--- src/Multiplication.java
+++ src/Multiplication.java
@@ -1,5 +1,7 @@
+//Multiplication class that gives us a method to multiple two numbers
 public class Multiplication {
-    public static int multiply(int a, int b) {
+    public static int multiply(int a, int b)
+    {
         return a * b;
     }
 }
