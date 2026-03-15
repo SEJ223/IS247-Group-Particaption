0f96969c2da2017c14be3e5efa73af60fe62add8
aaronsaudager <aaron16639@icloud.com>
2026-03-15 15:35:49 -0400

    Task 3 Complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Docker and Java!");
+        System.out.println("My name is Aaron. Nice to meet you!");
     }
 }
