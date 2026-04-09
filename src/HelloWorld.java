8bf342177ff01e613dab71a542fd2e637218cc2e
Gavin Williams <gavinw2@umbc.edu>
2026-04-09 16:33:20 -0400

    Group Task 5 - Step 1 (Comments)

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,18 +1,24 @@
-
-
-
+/**
+ *This is a public class HelloWorld
+ *
+ * @author Aaron
+ * @version 2025.3.2
+ *
+ *
+ */
 public class HelloWorld {
     /**
-    *This is the main method
-    *
-    * @param args allows the program to print the statement
-    *
+     *
+     * This is the main method
+     *
+     * @param args prints the code below
+     * @version 1.0
+     * @see Main
      */
     public static void main(String[] args) {
-
         /**
         *This code down here prints out Hello, docker file java!
-        *
+        *The other code prints My name is Aaron. Nice to meet you!
         *
         */
         System.out.println("Hello, Docker and Java!");
